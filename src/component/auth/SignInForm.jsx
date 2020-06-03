777cbb782cd989dfa9e47bf989854a39506b38a9
Georgii <corradowave1@gmail.com>
2020-06-03 18:04:20 +0300

    react loader

--- src/component/auth/SignInForm.jsx
+++ src/component/auth/SignInForm.jsx
@@ -1,8 +1,8 @@
 import React, {Component} from "react";
 import {Field, Form, Formik} from 'formik';
 import AuthService from "../../service/AuthService";
+import ReactLoading from "react-loading";
 import * as Yup from "yup";
-import LoadingSpinner from "../home/LoadingSpinner";
 
 class SignInForm extends Component {
 
@@ -64,50 +64,54 @@ class SignInForm extends Component {
     render() {
         let {loading, message, username, password} = this.state
         return (
-            <div className="form">
-                <h1>Sign in</h1>
-                <div className="signin-form">
-                    <Formik
-                        initialValues={{username, password}}
-                        onSubmit={this.onSubmit}
-                        validateOnChange={false}
-                        validateOnBlur={false}
-                        validate={this.validate}
-                        validationSchema={this.signinSchema}
-                        enableReinitialize={true}>
-                        {
-                            ({errors}) => (
-                                <Form>
-                                    <fieldset className="form-group">
-                                        <label>Username</label>
-                                        <Field className="form-control" type="text" name="username"/>
-                                    </fieldset>
-                                    {errors.username && (
-                                        <div className="alert alert-danger" role="alert">
-                                            {errors.username}
-                                        </div>)}
-                                    <fieldset className="form-group">
-                                        <label>Password</label>
-                                        <Field className="form-control" type="text" name="password"/>
-                                    </fieldset>
-                                    {errors.password && (
-                                        <div className="alert alert-danger" role="alert">
-                                            {errors.password}
-                                        </div>)}
-                                    {message && (
-                                        <div className="form-group">
-                                            <div className="alert alert-danger" role="alert">
-                                                {message}
-                                            </div>
-                                        </div>
-                                    )}
-                                    <button className="btn  btn-outline-light" type="submit">Log in</button>
-                                </Form>
-                            )
-                        }
-                    </Formik>
-                </div>
-                {loading ? <LoadingSpinner /> : null}
+            <div>
+                {loading ? (<ReactLoading type={"bars"} color={"white"}/>
+                ) : (
+                    <div className="form">
+                        <h1>Sign in</h1>
+                        <div className="signin-form">
+                            <Formik
+                                initialValues={{username, password}}
+                                onSubmit={this.onSubmit}
+                                validateOnChange={false}
+                                validateOnBlur={false}
+                                validate={this.validate}
+                                validationSchema={this.signinSchema}
+                                enableReinitialize={true}>
+                                {
+                                    ({errors}) => (
+                                        <Form>
+                                            <fieldset className="form-group">
+                                                <label>Username</label>
+                                                <Field className="form-control" type="text" name="username"/>
+                                            </fieldset>
+                                            {errors.username && (
+                                                <div className="alert alert-danger" role="alert">
+                                                    {errors.username}
+                                                </div>)}
+                                            <fieldset className="form-group">
+                                                <label>Password</label>
+                                                <Field className="form-control" type="text" name="password"/>
+                                            </fieldset>
+                                            {errors.password && (
+                                                <div className="alert alert-danger" role="alert">
+                                                    {errors.password}
+                                                </div>)}
+                                            {message && (
+                                                <div className="form-group">
+                                                    <div className="alert alert-danger" role="alert">
+                                                        {message}
+                                                    </div>
+                                                </div>
+                                            )}
+                                            <button className="btn  btn-outline-light" type="submit">Log in</button>
+                                        </Form>
+                                    )
+                                }
+                            </Formik>
+                        </div>
+                    </div>
+                )}
             </div>
         )
     }
